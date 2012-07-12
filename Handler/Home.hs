c4c286b0e8c4a636cf6ea4c6e4ce7498fd2e6208
Jean-Baptiste Mazon <jb@darkstar>
2012-07-12 23:35:31 +0200

    Results of the Riviera.rb July drink-up live coding session

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,6 +2,7 @@
 module Handler.Home where
 
 import Import
+import Yesod.Form.Nic
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -37,3 +38,27 @@ sampleForm :: Form (FileInfo, Text)
 sampleForm = renderDivs $ (,)
     <$> fileAFormReq "Choose a file"
     <*> areq textField "What's on the file?" Nothing
+
+postCreateForm :: Form Post
+postCreateForm = renderDivs $ Post
+    <$> areq textField "Post title" Nothing
+    <*> areq nicHtmlField "Post content" Nothing
+
+getBlogR :: Handler RepHtml
+getBlogR = do
+  posts <- runDB (selectList [] [])
+  (widget,enctype) <- generateFormPost postCreateForm
+  defaultLayout $(widgetFile "blog")
+
+postBlogR :: Handler RepHtml
+postBlogR = do
+  ((result,widget),enctype) <- runFormPost postCreateForm
+  case result of
+    FormSuccess post -> do
+      postId <- runDB $ insert post
+      redirect (PostR postId)
+
+getPostR :: PostId -> Handler RepHtml
+getPostR id = do
+  post <- runDB (get404 id)
+  defaultLayout $(widgetFile "post")
